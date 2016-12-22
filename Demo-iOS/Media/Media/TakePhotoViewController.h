4e1057f6d13e3f059c0669d6c865a88a3e38c00a
mrhuangyuhui <mrhuangyuhui@gmail.com>
2016-12-22 17:06:46 +0800

    学习使用AVFoundation框架进行拍照

---
+++ Demo-iOS/Media/Media/TakePhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  TakePhotoViewController.h
+//  Media
+//
+//  Created by Yuhui Huang on 12/22/16.
+//  Copyright © 2016 Yuhui Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TakePhotoViewController : UIViewController
+
+@end
