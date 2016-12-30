c50b45de151dea79426cb432122ad98207120de2
mrhuangyuhui <mrhuangyuhui@gmail.com>
2016-12-30 19:11:12 +0800

    study the source code of Aspects

--- Demo-iOS/Demo-iOS/Demo-iOS/AspectsDemo.m
+++ Demo-iOS/Demo-iOS/Demo-iOS/AspectsDemo.m
@@ -14,31 +14,41 @@
 
 @implementation AspectsDemo
 
-+ (void)load {
-    
-    [AspectsDemo aspect_hookSelector:@selector(viewWillAppear:)
-                         withOptions:AspectPositionBefore
-                          usingBlock:^(id<AspectInfo> info, BOOL animated) {
-                              NSLog(@"%@----%@", [[info instance] class], NSStringFromSelector([[info originalInvocation] selector]));
-                          }
-                               error:NULL];
-    
-    [AspectsDemo aspect_hookSelector:@selector(viewDidLoad)
-                         withOptions:AspectPositionAfter
-                          usingBlock:^(id<AspectInfo> info) {
-                              NSLog(@"%@----%@", [[info instance] class], NSStringFromSelector([[info originalInvocation] selector]));
-                          }
-                               error:NULL];
-}
+//+ (void)load {
+//    
+//    [AspectsDemo aspect_hookSelector:@selector(viewWillAppear:)
+//                         withOptions:AspectPositionBefore
+//                          usingBlock:^(id<AspectInfo> info, BOOL animated) {
+//                              NSLog(@"%@----%@", [[info instance] class], NSStringFromSelector([[info originalInvocation] selector]));
+//                          }
+//                               error:NULL];
+//    
+//    [AspectsDemo aspect_hookSelector:@selector(viewDidLoad)
+//                         withOptions:AspectPositionAfter
+//                          usingBlock:^(id<AspectInfo> info) {
+//                              NSLog(@"%@----%@", [[info instance] class], NSStringFromSelector([[info originalInvocation] selector]));
+//                          }
+//                               error:NULL];
+//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"viewDidLoad");
+    [self aspect_hookSelector:@selector(helloAspects)
+                  withOptions:AspectPositionAfter
+                   usingBlock:^(id<AspectInfo> info) {
+                       NSLog(@"after helloAspects");
+                   }
+                        error:NULL];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     NSLog(@"viewWillAppear");
+}
+
+- (void)helloAspects {
+    NSLog(@"helloAspects");
 }
 
 @end
