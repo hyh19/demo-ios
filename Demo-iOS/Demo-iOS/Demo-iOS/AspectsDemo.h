189658e25a4db5f6eb5cd55099658dc7c89ae395
mrhuangyuhui <mrhuangyuhui@gmail.com>
2016-12-30 12:56:04 +0800

    add aspects demo

---
+++ Demo-iOS/Demo-iOS/Demo-iOS/AspectsDemo.h
@@ -0,0 +1,13 @@
+//
+//  AspectsDemo.h
+//  Demo-iOS
+//
+//  Created by Yuhui Huang on 30/12/2016.
+//  Copyright © 2016 Yuhui Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AspectsDemo : UIViewController
+
+@end
---
+++ Demo-iOS/Demo-iOS/Demo-iOS/AspectsDemo.m
@@ -0,0 +1,44 @@
+//
+//  AspectsDemo.m
+//  Demo-iOS
+//
+//  Created by Yuhui Huang on 30/12/2016.
+//  Copyright © 2016 Yuhui Huang. All rights reserved.
+//
+
+#import "AspectsDemo.h"
+
+@interface AspectsDemo ()
+
+@end
+
+@implementation AspectsDemo
+
++ (void)load {
+    
+    [AspectsDemo aspect_hookSelector:@selector(viewWillAppear:)
+                         withOptions:AspectPositionBefore
+                          usingBlock:^(id<AspectInfo> info, BOOL animated) {
+                              NSLog(@"%@----%@", [[info instance] class], NSStringFromSelector([[info originalInvocation] selector]));
+                          }
+                               error:NULL];
+    
+    [AspectsDemo aspect_hookSelector:@selector(viewDidLoad)
+                         withOptions:AspectPositionAfter
+                          usingBlock:^(id<AspectInfo> info) {
+                              NSLog(@"%@----%@", [[info instance] class], NSStringFromSelector([[info originalInvocation] selector]));
+                          }
+                               error:NULL];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"viewDidLoad");
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    NSLog(@"viewWillAppear");
+}
+
+@end
